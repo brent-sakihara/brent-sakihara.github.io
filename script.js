800429a836e33d60fd54bafb8245848b276367a8
Brent <brent.saki@gmail.com>
2021-05-05 16:20:29 -0700

    added scroll arrow

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-let d = new Date();
-document.body.append("Today's date is " + d);
+var upbutton = document.getElementById('arrow');
+var body = document.body;
+var documentElement = document.documentElement;
+var offset = 100, scrollPos, docHeight;
+
+docHeight = Math.max(body.scrollHeight, body.offsetHeight, documentElement.clientHeight, documentElement.scrollHeight, documentElement.offsetHeight);
+if(docHeight != 'undefined'){
+    offset = docHeight / 5;
+}
+
+window.addEventListener("scroll", function(event){
+    scrollPos = body.scrollTop || documentElement.scrollTop;
+
+    upbutton.className = (scrollPos > offset) ? 'visible' : "";
+});
+
+upbutton.addEventListener('click', function(event){
+    event.preventDefault();
+    documentElement.scrollTop = 0;
+})
